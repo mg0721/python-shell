46c2957098f08a6f07025481c7e832983e4d721a
mg0721 <64mingn@gmail.com>
2021-04-01 01:42:06 +0900

    Add basic structure

---
+++ pyshell/__init__.py
@@ -0,0 +1,1 @@
+from pyshell.shell import Shell
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='pyshell',
+    version='0.0.1',
+    packages=find_packages(),
+    )
